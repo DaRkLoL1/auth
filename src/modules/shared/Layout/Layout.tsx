c597f92fa1713443129e037a98e16af7eaf3d6a8
Igor <darklol_777@mail.ru>
2020-04-16 16:31:59 +0300

    #17 delete top links

--- src/modules/shared/Layout/Layout.tsx
+++ src/modules/shared/Layout/Layout.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import block from 'bem-cn';
-import { Link } from 'react-router-dom';
 
-import { routes } from '../../routes';
 import './Layout.scss';
 
 const b = block('layout');
@@ -12,51 +10,9 @@ interface IProps {
 }
 
 const Layout = (props: IProps) => {
-  const getLinks = () => {
-    const linkTitles = [
-      {
-        title: 'Account',
-        href: routes.auth.account.getRoutePath(),
-        id: 1,
-      },
-      {
-        title: 'Sign In',
-        href: routes.auth.signIn.getRoutePath(),
-        id: 2,
-      },
-      {
-        title: 'Sign Up',
-        href: routes.auth.signUp.getRoutePath(),
-        id: 3,
-      },
-      {
-        title: 'Restore',
-        href: routes.auth.restore.getRoutePath(),
-        id: 4,
-      },
-    ];
-
-    return (
-      <>
-        {linkTitles.map(link => (
-          <li className={b('item')} key={link.id}>
-            <Link className={b('link')} to={link.href}>{link.title}</Link>
-          </li>
-        ))}
-      </>
-    );
-  };
-
   const { children } = props;
   return (
     <div className={b()}>
-      <header className={b('header')}>
-        <nav className={b('nav')}>
-          <ul className={b('list')}>
-            {getLinks()}
-          </ul>
-        </nav>
-      </header>
       <main className={b('content')}>
         <div className={b('form')}>
           {children}
